9af82c25e9eeb4832dd86c5b467fb6f495fcd584
linleyj <linley.jesson@plantandfood.co.nz>
2018-07-23 08:06:26 +1200

    scrape iPlant list

--- Day_2/DataVisualization/Part_2/examples/template1/global.R
+++ Day_2/DataVisualization/Part_2/examples/template1/global.R
@@ -1,7 +1,14 @@
 library(shiny)
 
-
+library(leaflet)
+library(magrittr)
+library(ggplot2)
+library(ggthemes)
+library(plotly)
+library(dplyr)
+library(rbokeh)
 ## -------- Global functions to abstract code from shiny application -------- #
 
 ## Identity function example
 x <- function(x)x
+
